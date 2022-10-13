bcfaa4ee1b15f6510e96139e302bef14d2870405
nahnahAJ <nahnahahjuwah@gmail.com>
2022-10-13 09:19:45 +0000

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,52 +1,51 @@
-import "./styles.css";
+import './styles.css';
 
-const formSubmit = document.getElementById("form");
-const refreshBtn = document.getElementById("refreshBtn");
-const scoresTab = document.querySelector(".scores-tab");
-const url =
-  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5o0ngsRqtDwdY7IseEIK/scores/";
+const formSubmit = document.getElementById('form');
+const refreshBtn = document.getElementById('refreshBtn');
+const scoresTab = document.querySelector('.scores-tab');
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/5o0ngsRqtDwdY7IseEIK/scores/';
 
-  //Sending scores to the API
-formSubmit.addEventListener("submit", (event) => {
+// Sending scores to the API
+formSubmit.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const formData = new FormData(formSubmit);
 
   // Extract data from formData
-  let extractedData = [...formData];
-  let userData = extractedData[0][1];
-  let scoreData = extractedData[1][1];
+  const extractedData = [...formData];
+  const userData = extractedData[0][1];
+  const scoreData = extractedData[1][1];
 
-  //Create an object with extracted Data
-  let scoreObject = {
+  // Create an object with extracted Data
+  const scoreObject = {
     user: userData,
     score: scoreData,
   };
 
   const sendScores = async () => {
     const res = await fetch(url, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(scoreObject),
     });
     const data = await res.json();
-    console.log("Data", data);
+    console.log('Data', data);
   };
   sendScores();
 });
 
-//Retrieving and Displaying Scores from the API
-refreshBtn.addEventListener("click", () => {
+// Retrieving and Displaying Scores from the API
+refreshBtn.addEventListener('click', () => {
   const getScores = async () => {
     const res = await fetch(url, {
-      method: "GET",
+      method: 'GET',
     });
     const data = await res.json();
     // Set the DOM container to empty before each render
-    scoresTab.innerHTML = "";
+    scoresTab.innerHTML = '';
     return data.result.forEach((recorded) => {
       //  create DOM elements and Append to display in the DOM
-      const list = document.createElement("li");
+      const list = document.createElement('li');
       list.innerHTML = `${recorded.user}: ${recorded.score}`;
       scoresTab.append(list);
     });
